25f6f49efed6303750a7389ead9050ff2405326e
Otto Söderman <ottoj.soderman@gmail.com>
2023-03-06 19:27:18 +0200

    Better mobile padding. Added title for event pages.

--- src/components/PageWrapper.tsx
+++ src/components/PageWrapper.tsx
@@ -4,6 +4,8 @@ interface Props {
   children: ReactNode
 }
 
-const PageWrapper = ({ children }: Props) => <div className="p-4 md:px-24 lg:px-48">{children}</div>
+const PageWrapper = ({ children }: Props) => (
+  <div className="px-8 md:px-24 lg:px-48">{children}</div>
+)
 
 export default PageWrapper
--- src/pages/events/[pid].tsx
+++ src/pages/events/[pid].tsx
@@ -1,4 +1,5 @@
 import { GetStaticPropsContext } from 'next'
+import Head from 'next/head'
 import Header from '../../components/Header'
 import Markdown from '../../components/Markdown'
 import PageWrapper from '../../components/PageWrapper'
@@ -12,12 +13,15 @@ type Props = {
 const Event = ({ event }: Props) => {
   return (
     <PageWrapper>
+      <Head>
+        <title>{event.name} - Aalto Gamers</title>
+      </Head>
       <Header>{event.name}</Header>
       <div className="flex flex-col items-center">
-        <div className="py-16 w-3/4">
-          <div className="flex">
+        <div className="py-16 md:w-3/4">
+          <div className="flex flex-col md:flex-row">
             <img src={event.image} alt={event.name} className="max-h-[350px] object-scale-down" />
-            <div className="pl-8 text-lightGray text-xl">
+            <div className="mt-8 md:mt-0 md:pl-8 text-lightGray text-xl text-center md:text-left">
               <Markdown>{event.tldr}</Markdown>
             </div>
           </div>
