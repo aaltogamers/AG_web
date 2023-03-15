72e97f1cc9e229405307960b08b0cd794b62af26
Otto Söderman <ottoj.soderman@gmail.com>
2023-03-15 14:52:47 +0200

    Added 404 page and fixed previewPage styles.

--- src/pages/admin.tsx
+++ src/pages/admin.tsx
@@ -12,7 +12,7 @@ const Admin = () => {
     ;(async () => {
       const CMS = (await import('netlify-cms-app')).default
       CMS.init()
-      CMS.registerPreviewStyle('https://aaltogamers.fi/_next/static/css/78e8bd7be338bb55.css')
+      CMS.registerPreviewStyle('/previewStyles.css')
       CMS.registerPreviewTemplate('landing info', LandingInfosPreview)
       CMS.registerPreviewTemplate('event', EventPreview)
       CMS.registerPreviewTemplate('partner', PartnerPreview)
