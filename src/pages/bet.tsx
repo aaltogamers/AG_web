b2d76e11c13ca826f9443708b2b74ffcbb2b7b92
Otto Söderman <ottoj.soderman@gmail.com>
2023-11-09 14:51:25 +0200

    Render main even when there is no poll

--- src/pages/bet.tsx
+++ src/pages/bet.tsx
@@ -56,11 +56,7 @@ const Vote = () => {
     }
   }, [])
 
-  if (!visiblePoll) {
-    return <></>
-  }
-
-  const countMap = generateCountMap(votesForPoll, visiblePoll)
+  const countMap = visiblePoll ? generateCountMap(votesForPoll, visiblePoll) : new Map()
 
   const colors = ['#262E70', '#F89E1B', '#F4D35E', '#70C1B3', '#1098F7', '#14281D']
 
@@ -87,7 +83,7 @@ const Vote = () => {
 
   const totalMaybe0 = counts.reduce((acc, { count }) => acc + count, 0)
 
-  const total = totalMaybe0 === 0 ? 1 : totalMaybe0
+  const total = totalMaybe0 === 0 ? 0.000001 : totalMaybe0
 
   return (
     <>
@@ -95,56 +91,62 @@ const Vote = () => {
         <title>Twitch Stream Betting - Aalto Gamers</title>
       </Head>
       <main className={`text-white flex flex-col text-4xl h-[${screenHeight}px]`}>
-        <div className={`h[${topTextHeight}px] flex justify-end`}>
-          <p className=" bg-transparentBlack p-2 w-fit text-5xl mx-2 my-2">
-            {visiblePoll.question}
-          </p>
-        </div>
-        {counts
-          .sort((a, b) => b.count - a.count)
-          .map(({ count, name, color }) => {
-            const percentage = (count / total) * 100
-            const isCorrectOption = visiblePoll.correctOption === name
-            const pollHasEnded = visiblePoll.correctOption !== undefined
-            let textClass = ''
-            let pointsText = ''
-            if (pollHasEnded) {
-              if (isCorrectOption) {
-                textClass = 'text-green-500'
-                pointsText = `+${visiblePoll.pointsForWin} pt`
-              } else {
-                textClass = 'text-red'
-                pointsText = `+0 pt`
-              }
-            }
+        {visiblePoll && (
+          <>
+            <div className={`h[${topTextHeight}px] flex justify-end`}>
+              <p className=" bg-transparentBlack p-2 w-fit text-5xl mx-2 my-2">
+                {visiblePoll.question}
+              </p>
+            </div>
+            {counts
+              .sort((a, b) => a.name.localeCompare(b.name))
+              .map(({ count, name, color }) => {
+                const percentage = (count / total) * 100
+                const isCorrectOption = visiblePoll.correctOption === name
+                const pollHasEnded = visiblePoll.correctOption !== undefined
+                let textClass = ''
+                let pointsText = ''
+                if (pollHasEnded) {
+                  if (isCorrectOption) {
+                    textClass = 'text-green-500'
+                    pointsText = `+${visiblePoll.pointsForWin || 0} pt`
+                  } else {
+                    textClass = 'text-red'
+                    pointsText = `+0 pt`
+                  }
+                }
 
-            return (
-              <div
-                className="flex items-center pl-8 justify-end "
-                style={{ marginBottom: marginHeight }}
-                key={name}
-              >
-                <div className={`mr-4 p-2 bg-transparentBlack text-center ${textClass}`}>
-                  <p>{name}</p>
-                  <p>{pointsText}</p>
-                </div>
-                <div
-                  className="flex items-center pl-4"
-                  style={{
-                    width: Math.max((percentage / 100) * screenWidth, 75),
-                    height: barHeight,
-                    backgroundColor: color,
-                  }}
-                >
-                  {percentage.toFixed(0)}%
-                </div>
-              </div>
-            )
-          })}
-        <p style={{ height: bottomTextHeight }} className="flex justify-end px-4 text-3xl">
-          {total} Bets.
-          {visiblePoll.isVotable ? ' Betting is open! Use the chat to bet.' : ' Betting is closed.'}
-        </p>
+                return (
+                  <div
+                    className="flex items-center pl-8 justify-end "
+                    style={{ marginBottom: marginHeight }}
+                    key={name}
+                  >
+                    <div className={`mr-4 p-2 bg-transparentBlack text-center ${textClass}`}>
+                      <p>{name}</p>
+                      <p>{pointsText}</p>
+                    </div>
+                    <div
+                      className="flex items-center pl-4 transition-all duration-[2000ms] ease-in-out"
+                      style={{
+                        width: Math.max((percentage / 100) * screenWidth, 75),
+                        height: barHeight,
+                        backgroundColor: color,
+                      }}
+                    >
+                      {percentage.toFixed(0)}%
+                    </div>
+                  </div>
+                )
+              })}
+            <p style={{ height: bottomTextHeight }} className="flex justify-end px-4 text-3xl">
+              {total.toFixed(0)} Bets.
+              {visiblePoll.isVotable
+                ? ' Betting is open! Use the chat to bet.'
+                : ' Betting is closed.'}
+            </p>
+          </>
+        )}
       </main>
     </>
   )
