f22b7378997efc1af9ac18fa051836f846a4d2c4
Otju <ottoj.soderman@gmail.com>
2024-03-24 17:37:27 +0200

    Fix padding issue with headers

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ interface Props {
 const Header = ({ children }: Props) => {
   return (
     <header className="mt-20 text-center flex flex-col items-center text-2xl ">
-      <h1 className="border-red border-b-8 px-24 pb-8">{children}</h1>
+      <h1 className="border-red border-b-8 px-8 pb-8">{children}</h1>
     </header>
   )
 }
