4ccea46d959f9a537b1abbdcc3373730d59f85fe
Otju <ottoj.soderman@gmail.com>
2025-02-11 22:30:55 +0200

    Basic roulette animation

--- src/pages/roulette.tsx
+++ src/pages/roulette.tsx
@@ -1,4 +1,7 @@
+'use client'
+
 /* eslint-disable @next/next/no-img-element */
+
 import Head from 'next/head'
 import { useEffect, useRef, useState } from 'react'
 
@@ -12,8 +15,22 @@ const Roulette = () => {
     'Zelda.png',
   ]
   const [patch, setPatch] = useState<string | null>(patchNames[0])
+  const [windowSize, setWindowSize] = useState({ width: 0, height: 0 })
+  const [speed, setSpeed] = useState(0)
+  const frameCount = useRef(0)
 
   const canvasRef = useRef<HTMLCanvasElement>(null)
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowSize({ width: window.innerWidth, height: window.innerHeight })
+    }
+    handleResize()
+    window.addEventListener('resize', handleResize)
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [])
 
   useEffect(() => {
     const canvas = canvasRef.current
@@ -25,24 +42,36 @@ const Roulette = () => {
       return
     }
 
-    let frameCount = 0
     let animationFrameId = 0
 
-    const draw = (ctx: CanvasRenderingContext2D, frameCount: number) => {
+    const draw = (ctx: CanvasRenderingContext2D) => {
       const { width, height } = ctx.canvas
       ctx.clearRect(0, 0, width, height)
       ctx.fillStyle = 'black'
       ctx.fillRect(0, 0, width, height)
       ctx.fillStyle = 'red'
-      const imageElement = document.getElementById(patch || '') as HTMLImageElement | null
-      if (imageElement) {
-        ctx.drawImage(imageElement, (frameCount * 4) % width, 0, 1982 / 5, 3259 / 5)
-      }
+      const imageElements = patchNames
+        .map((name) => document.getElementById(name) as HTMLImageElement | null)
+        .filter((element) => element !== null)
+
+      const scaleRatio = 8
+
+      const imageWidth = 1982 / scaleRatio
+      const imageHeight = 3259 / scaleRatio
+
+      const imageCount = imageElements.length
+      const gapWidth = 120
+      const totalWidth = (imageWidth + gapWidth) * imageCount
+
+      imageElements.forEach((imageElement, index) => {
+        const x = ((index * (imageWidth + gapWidth) + frameCount.current) % totalWidth) - imageWidth
+        ctx.drawImage(imageElement, x, 0, imageWidth, imageHeight)
+      })
     }
 
     const render = () => {
-      frameCount++
-      draw(context, frameCount)
+      frameCount.current += speed
+      draw(context)
       animationFrameId = window.requestAnimationFrame(render)
     }
     render()
@@ -50,17 +79,28 @@ const Roulette = () => {
     return () => {
       window.cancelAnimationFrame(animationFrameId)
     }
-  }, [])
+  }, [speed])
 
   return (
     <div>
       <Head>
         <title>Roulette - Aalto Gamers</title>
       </Head>
-      <canvas ref={canvasRef} width={window.innerWidth} height={window.innerHeight} />
-      {patchNames.map((name) => (
-        <img src={`/images/patches/${name}`} alt={name} key={name} className="hidden" id={name} />
-      ))}
+      <main className="relative">
+        <canvas ref={canvasRef} width={windowSize.width} height={windowSize.height} />
+        {patchNames.map((name) => (
+          <img src={`/images/patches/${name}`} alt={name} key={name} className="hidden" id={name} />
+        ))}
+        <div className="absolute bottom-40 w-full flex justify-center">
+          <input
+            type="range"
+            min="0"
+            max="10"
+            value={speed}
+            onChange={(e) => setSpeed(Number(e.target.value))}
+          />
+        </div>
+      </main>
     </div>
   )
 }
