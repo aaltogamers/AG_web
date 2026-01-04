b5fad5282b7ecffcec5e3e6dbc9f53df8804ec73
Otju <ottoj.soderman@gmail.com>
2026-01-04 20:49:19 +0200

    Fix images

--- src/components/BoardMember.tsx
+++ src/components/BoardMember.tsx
@@ -8,7 +8,11 @@ type Props = {
 const BoardMember = ({ boardMember, showContactInfo }: Props) => {
   return (
     <div className="flex flex-col align-center m-8 text-center">
-      <AGImage src={boardMember.image || '/images/board-placeholder.png'} alt={boardMember.name} />
+      <AGImage
+        src={boardMember.image || '/images/board-placeholder.png'}
+        alt={boardMember.name}
+        className="aspect-3/4 object-cover"
+      />
       {boardMember.title && <h3 className="mt-4">{boardMember.title}</h3>}
       <h4>{boardMember.name}</h4>
       <div className="mt-4">
