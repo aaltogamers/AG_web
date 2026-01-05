6b5dd9025104420a139a152a92a49484e0005474
Otju <ottoj.soderman@gmail.com>
2026-01-06 00:36:44 +0200

    Fix error

--- src/components/Event.tsx
+++ src/components/Event.tsx
@@ -12,7 +12,11 @@ const Event = ({ event }: Props) => {
     <div className="flex flex-col md:w-3/4 " key={event.name}>
       <hr className="bg-gray w-full my-16" />
       <div className="md:grid md:grid-cols-event md:flex-row text-center md:text-left items-center">
-        <AGImage src={event.image} alt={event.name} className="max-h-96 object-contain" />
+        <AGImage
+          src={event.image || '/images/ag-white.png'}
+          alt={event.name}
+          className="max-h-96 object-contain"
+        />
         <div className="flex flex-col md:p-10 items-center md:items-start">
           <h3 className="mt-8 md:mt-0">{event.name}</h3>
           <Markdown>{event.description}</Markdown>
