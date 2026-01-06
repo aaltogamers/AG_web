470a3639a40236cd9616f5b4b83e74ae6c54cc3f
Otju <ottoj.soderman@gmail.com>
2026-01-06 21:41:18 +0200

    Make socials page better

--- src/pages/join.tsx
+++ src/pages/join.tsx
@@ -13,7 +13,7 @@ const Join = () => {
       <Header>Join us</Header>
       <h3 className="w-full text-center mt-10">Be a part of the coolest gaming community!</h3>
       <div className="w-full justify-center flex">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 w-3/4">
+        <div className="flex flex-row flex-wrap justify-center gap-8 gap-x-16 w-3/4 mt-4">
           {medias.map(({ name, link, Icon }) => (
             <Link href={link} key={name} className="flex items-center justify-center text-xl">
               {name}
