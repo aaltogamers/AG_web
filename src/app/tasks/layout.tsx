385ba07c009c632d27c54b5ae5a3d11fc294416d
Otju <ottoj.soderman@gmail.com>
2026-06-16 01:57:59 +0300

    Hide loading thing

--- src/app/tasks/layout.tsx
+++ src/app/tasks/layout.tsx
@@ -4,6 +4,7 @@ import TelegramProvider from '../../components/tasks/TelegramProvider'
 export default function TasksLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
+      <style dangerouslySetInnerHTML={{ __html: 'nav, footer, .tg-hide { display: none !important; }' }} />
       <Script
         src="https://telegram.org/js/telegram-web-app.js"
         strategy="afterInteractive"
