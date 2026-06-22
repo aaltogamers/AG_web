a3f2f3d4dfd56ef4d203542f4d8f7bfd108aacce
Otju <ottoj.soderman@gmail.com>
2026-06-22 22:06:25 +0300

    Fix lists

--- src/utils/markdownLinks.tsx
+++ src/utils/markdownLinks.tsx
@@ -2,10 +2,7 @@ import React from 'react'
 import ReactMarkdown, { Components } from 'react-markdown'
 
 export function markdownToTelegramHtml(text: string): string {
-  const escaped = text
-    .replace(/&/g, '&amp;')
-    .replace(/</g, '&lt;')
-    .replace(/>/g, '&gt;')
+  const escaped = text.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;')
   return escaped
     .replace(/\*\*(.+?)\*\*/g, '<b>$1</b>')
     .replace(/(?<!\*)\*(?!\*)(.+?)(?<!\*)\*(?!\*)/g, '<i>$1</i>')
@@ -31,16 +28,18 @@ const components: Components = {
   em: ({ children }) => <em>{children}</em>,
   del: ({ children }) => <s>{children}</s>,
   code: ({ children }) => (
-    <code className="px-1 rounded" style={{ backgroundColor: 'var(--tg-theme-secondary-bg-color, #f0f0f0)' }}>
+    <code
+      className="px-1 rounded"
+      style={{ backgroundColor: 'var(--tg-theme-secondary-bg-color, #f0f0f0)' }}
+    >
       {children}
     </code>
   ),
+  ul: ({ children }) => <ul className="list-disc list-inside mt-4 mb-8">{children}</ul>,
+  ol: ({ children }) => <ol className="list-decimal list-inside mt-4 mb-8">{children}</ol>,
+  li: ({ children }) => <li className="mb-2">{children}</li>,
 }
 
 export function DescriptionMarkdown({ text }: { text: string }) {
-  return (
-    <ReactMarkdown components={components}>
-      {text}
-    </ReactMarkdown>
-  )
+  return <ReactMarkdown components={components}>{text}</ReactMarkdown>
 }
